4bc6af07da079eba47c89efcfc7f4c393be15aa8
Asker <kotsevasker0@gmail.com>
2024-12-04 19:53:06 +0300

    creaet module menu

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	menu "github.com/Asker231/todo-cli.git/internal/menu"
+	term "github.com/nsf/termbox-go"
+)
 func main() {
-	
+	sel := 1;
+	err := term.Init()
+	if err != nil{
+		fmt.Println(err)
+	}
+	defer term.Close()
+	menu.Select(sel)
 }
+
