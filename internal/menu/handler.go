477ee7a25e260a1b21f5fd3f0197a8368b979acb
Asker <kotsevasker0@gmail.com>
2024-12-04 19:55:25 +0300

    creaet module menu

--- internal/menu/handler.go
+++ internal/menu/handler.go
@@ -5,59 +5,63 @@ import (
 	"github.com/fatih/color"
 	term "github.com/nsf/termbox-go"
 )
-func reset(){
+
+func reset() {
 	term.Sync()
 }
 
-func Select(sel int){
+func Select(sel int) {
 	Menu(sel)
-	loop:
-	for{
+loop:
+	for {
 		ev := term.PollEvent()
-		switch ev.Type{
+		switch ev.Type {
 		case term.EventKey:
-			switch ev.Key{
+			switch ev.Key {
 			case term.KeyArrowDown:
-				if sel > 2 {sel = 2}
+				if sel > 2 {
+					sel = 2
+				}
 				sel += 1
 				reset()
 				Menu(sel)
 			case term.KeyArrowUp:
-				if sel <= 1{sel += 1}
+				if sel <= 1 {
+					sel += 1
+				}
 				sel -= 1
 				reset()
 				Menu(sel)
 			default:
 				reset()
-				break loop		
+				break loop
 			}
 		}
 	}
 }
 
-
-	func Menu(sel int){
-		if sel == 1{
-			color.Cyan("Add todo: ➕\n")
-			fmt.Print("\n")
-			fmt.Print("Delete todo:🗑️\n")
-			fmt.Print("\n")
-			fmt.Print("List:📋\n")
-		}
-
-		if sel == 2{
-			fmt.Print("Add todo: ➕\n")
-			fmt.Print("\n")
-			color.Cyan("Delete todo:🗑️\n")
-			fmt.Print("\n")
-			fmt.Print("List:📋\n")
-		}
-
-		if sel == 3{
-			fmt.Print("Add todo: ➕\n")
-			fmt.Print("\n")
-			fmt.Print("Delete todo:🗑️\n")
-			fmt.Print("\n")
-			color.Cyan("List:📋\n")
-		}
+func Menu(sel int) {
+	if sel == 1 {
+		color.Cyan("Add todo: ➕\n")
+		fmt.Print("\n")
+		fmt.Print("Delete todo:🗑️\n")
+		fmt.Print("\n")
+		fmt.Print("List:📋\n")
 	}
+
+	if sel == 2 {
+		fmt.Print("Add todo: ➕\n")
+		fmt.Print("\n")
+		color.Cyan("Delete todo:🗑️\n")
+		fmt.Print("\n")
+		fmt.Print("List:📋\n")
+	}
+
+	if sel == 3 {
+		fmt.Print("Add todo: ➕\n")
+		fmt.Print("\n")
+		fmt.Print("Delete todo:🗑️\n")
+		fmt.Print("\n")
+		color.Cyan("List:📋\n")
+	}
+}
