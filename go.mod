858c188f2650020ee3baf69f6374c42f619976cf
Asker <kotsevasker0@gmail.com>
2024-12-04 19:54:28 +0300

    creaet module menu

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/Asker231/todo-cli.git
 
 go 1.23.2
+
+require (
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
---
+++ internal/menu/handler.go
@@ -0,0 +1,63 @@
+package menu
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	term "github.com/nsf/termbox-go"
+)
+func reset(){
+	term.Sync()
+}
+
+func Select(sel int){
+	Menu(sel)
+	loop:
+	for{
+		ev := term.PollEvent()
+		switch ev.Type{
+		case term.EventKey:
+			switch ev.Key{
+			case term.KeyArrowDown:
+				if sel > 2 {sel = 2}
+				sel += 1
+				reset()
+				Menu(sel)
+			case term.KeyArrowUp:
+				if sel <= 1{sel += 1}
+				sel -= 1
+				reset()
+				Menu(sel)
+			default:
+				reset()
+				break loop		
+			}
+		}
+	}
+}
+
+
+	func Menu(sel int){
+		if sel == 1{
+			color.Cyan("Add todo: ➕\n")
+			fmt.Print("\n")
+			fmt.Print("Delete todo:🗑️\n")
+			fmt.Print("\n")
+			fmt.Print("List:📋\n")
+		}
+
+		if sel == 2{
+			fmt.Print("Add todo: ➕\n")
+			fmt.Print("\n")
+			color.Cyan("Delete todo:🗑️\n")
+			fmt.Print("\n")
+			fmt.Print("List:📋\n")
+		}
+
+		if sel == 3{
+			fmt.Print("Add todo: ➕\n")
+			fmt.Print("\n")
+			fmt.Print("Delete todo:🗑️\n")
+			fmt.Print("\n")
+			color.Cyan("List:📋\n")
+		}
+	}
